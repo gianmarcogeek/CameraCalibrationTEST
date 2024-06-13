168e3f0fc964fe015197012840d5ff656363945c
Gianmarco <gianmarcogeek@gmail.com>
2024-06-13 18:18:51 +0200

    getimages

--- getImages.py
+++ getImages.py
@@ -1,6 +1,33 @@
 import cv2
 
-cap = cv2.VideoCapture(2)
+def gstreamer_pipeline(
+    sensor_id=1,
+    capture_width=3840,
+    capture_height=2160,
+    display_width=3840,
+    display_height=2160,
+    framerate=10,
+    flip_method=2,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+cap = cv2.VideoCapture(gstreamer_pipeline())
 
 num = 0
 
